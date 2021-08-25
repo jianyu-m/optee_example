5ada502106f5eca81a5d1ab837f4c59599252379
jianyu-m <jianyu@hku.hk>
2021-08-25 16:00:02 +0800

    gpu enclave

---
+++ gpu_hello_world/ta/sub.mk
@@ -0,0 +1,7 @@
+global-incdirs-y += include
+srcs-y += hello_world_ta.c
+srcs-y += matrixadd.c
+srcs-y += main.c
+
+# To remove a certain compiler flag, add a line like this
+#cflags-template_ta.c-y += -Wno-strict-prototypes
